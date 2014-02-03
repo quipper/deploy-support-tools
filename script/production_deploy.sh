80839ccb5abb394612ff7b20e5e75a9d242e99d0
Tomo Nakano <tomo@quipper.com>
2014-02-03 03:08:38 +0000

    HEROKU_APPS should be set before the deploy script loading

--- script/production_deploy.sh
+++ script/production_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
+HEROKU_APPS="quipper-heroku-support-tools"
+
 DEPLOY_SCRIPT=/tmp/deploy.$$.sh
 curl https://quipper-deploy-support-tools.herokuapp.com/scripts/production_deploy.sh.txt > ${DEPLOY_SCRIPT}
 . ${DEPLOY_SCRIPT}
-
-HEROKU_APPS="quipper-heroku-support-tools"
 
 deploy
