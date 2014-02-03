e81d0a4969a138a99e9da91da5d9967b4a6a22d2
Tomo Nakano <tomo@quipper.com>
2014-02-03 03:10:00 +0000

    renamed

--- script/production_deploy.sh
+++ script/production_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-HEROKU_APPS="quipper-heroku-support-tools"
+HEROKU_APPS="quipper-deploy-support-tools"
 
 DEPLOY_SCRIPT=/tmp/deploy.$$.sh
 curl https://quipper-deploy-support-tools.herokuapp.com/scripts/production_deploy.sh.txt > ${DEPLOY_SCRIPT}
