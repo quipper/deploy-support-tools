94d948b8cff9542f759c95fbc658b229e6552413
Yuki MIYAUCHI <fistfvck@gmail.com>
2014-03-17 12:18:55 +0900

    add auth helper

---
+++ spec/support/auth_helper.rb
@@ -0,0 +1,12 @@
+module AuthHelper
+  def http_auth_header
+    {
+      'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(ENV["AUTH_USER"], ENV["AUTH_PASSWORD"])
+    }
+  end
+end
+
+RSpec.configure do |config|
+  config.include AuthHelper, type: :request
+  config.include AuthHelper, type: :controller
+end
