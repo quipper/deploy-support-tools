99448139157361c6867242b32a1eaf6fcb8e1033
Yuki MIYAUCHI <yuki@quipper.com>
2014-10-28 20:51:36 +0900

    extract to model

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -17,36 +17,10 @@ class AppsController < ApplicationController
     apps = apps.sort.uniq
     Rails.cache.write(APPS_KEY, apps)
 
-    cache_key = "app-#{app}"
-    keys = Rails.cache.read(cache_key) || []
+    kiosk = App.new app, servers
+    app_number = kiosk.lottery branch
+    kiosk.save
 
-    # keys structure
-    # [{"branch_name": "feacher-foo",
-    #   "last_use": <timestamp>},
-    #  {"branch_name": "feacher-bar",
-    #   "last_use": <timestamp>}]
-
-    keys = keys[0..(servers-1)] # when number of servers is decreased
-
-    if index = keys.index{|key| key["branch_name"] == branch }
-      # found
-      app_number = index + 1
-      keys[index]["last_use"] = Time.now
-    elsif keys.length < servers
-      # space available
-      keys << {"branch_name" => branch,
-        "last_use" => Time.now}
-      app_number = keys.length
-    else
-      # no space available. recycle
-      oldest = keys.sort{|a, b| a["last_use"] <=> b["last_use"]}.first
-      index = keys.index(oldest)
-      app_number = index + 1
-      keys[index]["branch_name"] = branch
-      keys[index]["last_use"] = Time.now
-    end
-
-    keys = Rails.cache.write(cache_key, keys)
     render text: "#{app}-staging-#{app_number}"
   end
 end
