f448b75e802a610ae2007d9caea42563ab4a9983
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-11-05 17:16:31 +0900

    Save RepoConfig

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -26,10 +26,36 @@ describe AppsController do
   end
 
   describe "#create" do
-    it "responds successfully with an HTTP 200 status code and app name" do
-      post :create, app: "foo", branch: "feature-bar"
-      expect(response).to be_success
-      expect(response.body).to eql("foo-staging-1")
+    let(:store) { ActiveSupport::Cache::MemoryStore.new }
+
+    before do
+      allow(Rails).to receive(:cache) { store }
+    end
+
+    context 'without repo_name' do
+      before do
+        post :create, app: "foo", branch: "feature-bar"
+      end
+
+      it "responds successfully with an HTTP 200 status code and app name" do
+        expect(response).to be_success
+        expect(response.body).to eql("foo-staging-1")
+      end
+    end
+
+    context 'with repo_name' do
+      before do
+        post :create, app: "foo", branch: "feature-bar", servers: 10, repo_name: "quipper/foo"
+      end
+
+      it "responds successfully with an HTTP 200 status code and app name" do
+        expect(response).to be_success
+        expect(response.body).to eql("foo-staging-1")
+      end
+
+      it "persists repo config into Rails.cache" do
+        expect(store.read('repo_config-quipper/foo')).to eq({app_prefix: 'foo', max_entries: 10})
+      end
     end
   end
 end
