e3fde8fef46242463a76c8dd3e270e5a3246bbc0
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-11-05 16:55:36 +0900

    Fix test descriptions

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe AppsController do
-  describe 'index' do
+  describe '#index' do
     before do
       http_login
     end
@@ -25,8 +25,8 @@ describe AppsController do
     end
   end
 
-  describe "Find or create app name" do
-    it "responds successfully with an HTTP 200 status code" do
+  describe "#create" do
+    it "responds successfully with an HTTP 200 status code and app name" do
       post :create, app: "foo", branch: "feature-bar"
       expect(response).to be_success
       expect(response.body).to eql("foo-staging-1")
