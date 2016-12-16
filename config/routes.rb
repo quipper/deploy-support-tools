fb0afd8a109056a5693dd3b92b07caceb39192a8
Kohei Hasegawa <ameutau@gmail.com>
2016-12-16 18:05:05 +0900

    Remove hipchat
    We've migrated to slack.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ HerokuSupportTools::Application.routes.draw do
   end
   get '/scripts/production_deploy.sh(.:format)', to: 'scripts#production_deploy'
   get '/scripts/staging_deploy.sh(.:format)', to: 'scripts#staging_deploy'
-  post '/notifications/hipchat', to: 'notifications#hipchat'
 
   resources :deployments, only: [:create]
 end
--- spec/integration/scripts_spec.rb
+++ spec/integration/scripts_spec.rb
@@ -20,7 +20,6 @@ describe ScriptsController do
       expect(response.body).to include('function deploy()')
       expect(response.body).to include('http://my-deploy-tool-url.herokuapp.com/apps')
       expect(response.body).to include('function notify()')
-      expect(response.body).to include('http://my-deploy-tool-url.herokuapp.com/notifications/hipchat')
     end
   end
 end
